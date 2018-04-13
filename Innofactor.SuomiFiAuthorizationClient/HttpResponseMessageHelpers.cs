e7756342f6390d322fbbf9de0967afe8d949ffa6
jukka.hyvarinen <jukka.hyvarinen@innofactor.com>
2018-04-13 15:09:59 +0300

    initial commit

---
+++ Innofactor.SuomiFiAuthorizationClient/HttpResponseMessageHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Innofactor.SuomiFiAuthorizationClient {
+    public static class HttpResponseMessageHelpers {
+
+        /// <summary>
+        /// Ensures that HTTP response message contains success status code and throws <see cref="HttpStatusCodeException"/> if not.
+        /// </summary>
+        /// <param name="message">HTTP response message.</param>
+        /// <remarks>Using this method ensures that the status code and reason phrase are captured to the exception.</remarks>
+        public static void EnsureSuccess(this HttpResponseMessage message) {
+            try {
+                message.EnsureSuccessStatusCode();
+            } catch (HttpRequestException x) {
+                throw new HttpStatusCodeException(message.StatusCode, message.ReasonPhrase, x);
+            }
+        }
+
+    }
+
+}
---
+++ Innofactor.SuomiFiAuthorizationClient/WebApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Innofactor.SuomiFiAuthorizationClient {
+    public class WebApiClient {
+
+        private readonly HttpClient httpClient;
+
+        private async Task<T> GetObject<T>(HttpRequestMessage request) where T : class {
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccess();
+
+            var result = await ReadObject<T>(response);
+            return result;
+
+        }
+
+        private async Task<T> ReadObject<T>(HttpResponseMessage response) where T : class {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            var stream = await response.Content.ReadAsStreamAsync();
+            return serializer.ReadObject(stream) as T;
+        }
+    }
+}
